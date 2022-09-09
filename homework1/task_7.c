caf992781c0244bb6ac96b0bf35c1d4765d28fe1
TimaFrolov <tima1911@yandex.ru>
2022-09-09 19:17:28 +0300

    Completed homework 1 task 7

---
+++ homework1/task_7.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+const int SQRT_OF_MAX_INT = 46340;
+
+int main()
+{
+    int maxNumber;
+    printf("Enter number: ");
+    scanf("%d", &maxNumber);
+    maxNumber++;
+
+    bool *isPrime = malloc(maxNumber * sizeof(bool));
+    for (int i = 2; i < maxNumber; ++i)
+    {
+        isPrime[i] = 1;
+    }
+    isPrime[0] = isPrime[1] = 0;
+
+    for (int number = 2; number < maxNumber; ++number)
+    {
+        if (isPrime[number])
+        {
+            printf("%d ", number);
+            if (number < SQRT_OF_MAX_INT) // checking that number^2 will be positive
+            {
+                for (int numberMultiple = number * number; numberMultiple < maxNumber; numberMultiple += number)
+                {
+                    isPrime[numberMultiple] = 0;
+                }
+            }
+        }
+    }
+    free(isPrime);
+    printf("\n");
+}
