485dcc0e1b3e61139a0eb10098663614fd2d140d
TimaFrolov <tima1911@yandex.ru>
2022-09-18 17:26:30 +0300

    Completed hw 2 task 1

---
+++ homework2/task1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define uint unsigned int
+
+uint rfib(uint n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    if (n <= 2)
+    {
+        return 1;
+    }
+
+    return rfib(n - 1) + rfib(n - 2);
+}
+
+uint ifib(uint n)
+{
+    uint latestNumber = 0, preLatestNumber = 1;
+
+    for (uint i = 0; i < n; ++i)
+    {
+        latestNumber = latestNumber + preLatestNumber;
+        preLatestNumber = latestNumber - preLatestNumber;
+    }
+
+    return latestNumber;
+}
+
+int main()
+{
+    while (1)
+    {
+        uint n = 0;
+
+        printf("Enter positive number N to get Nth fibonacci number (or enter 0 to exit program): ");
+        int scanResult = scanf("%u", &n);
+        while (scanResult == 0)
+        {
+            scanf("%*[^\n]");
+            printf("Wrong input! Please enter positive number: ");
+            scanResult = scanf("%u", &n);
+        }
+
+        if (n == 0)
+        {
+            break;
+        }
+
+        const clock_t clockAtStartIt = clock();
+        const uint ansIt = ifib(n);
+        const clock_t clockAtEndIt = clock();
+
+        printf("Iterative method took %.3f seconds! Answer is %u\n", (float)(clockAtEndIt - clockAtStartIt) / CLOCKS_PER_SEC, ansIt);
+
+        const clock_t clockAtStartRe = clock();
+        const uint ansRe = rfib(n);
+        const clock_t clockAtEndRe = clock();
+
+        printf("Recursive method took %.3f seconds! Answer is %u\n", (float)(clockAtEndRe - clockAtStartRe) / CLOCKS_PER_SEC, ansRe);
+    }
+    return 0;
+}
