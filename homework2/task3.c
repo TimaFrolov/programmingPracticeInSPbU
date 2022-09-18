ac274b2e3daf423d2a69fee8787ef9e1f6ff9857
TimaFrolov <tima1911@yandex.ru>
2022-09-18 18:56:06 +0300

    Completed hw 2 task 3

---
+++ homework2/task3.c
@@ -0,0 +1,184 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define arrType int
+
+// #define DEBUG 1
+
+void swap(arrType *a, arrType *b)
+{
+    arrType tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void bubbleSort(size_t len, arrType *arr)
+{
+    for (size_t i = 0; i < len; ++i)
+    {
+        for (size_t j = i + 1; j < len; ++j)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(&arr[i], &arr[j]);
+            }
+        }
+    }
+}
+
+void countSort(size_t len, arrType *arr)
+{
+    arrType *sortedArr = malloc(sizeof(arrType) * len);
+
+    for (size_t i = 0; i < len; ++i)
+    {
+        size_t c = 0;
+
+        for (size_t j = 0; j < i; ++j)
+        {
+            if (arr[j] <= arr[i])
+            {
+                ++c;
+            }
+        }
+
+        for (size_t j = i + 1; j < len; ++j)
+        {
+            if (arr[j] < arr[i])
+            {
+                ++c;
+            }
+        }
+
+        sortedArr[c] = arr[i];
+    }
+
+    for (size_t i = 0; i < len; ++i)
+    {
+        arr[i] = sortedArr[i];
+    }
+    free(sortedArr);
+}
+
+#ifdef DEBUG
+int test()
+{
+    int testResult = 0;
+
+    arrType arr[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    bubbleSort(10, arr);
+    int isTestFailed = 0;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        if (arr[i] != i + 1)
+        {
+            isTestFailed = 1;
+            break;
+        }
+    }
+
+    if (isTestFailed)
+    {
+        printf("Test failed! Initial array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}. Array after bubble sort: {");
+        for (int i = 0; i < 9; ++i)
+        {
+            printf("%d, ", arr[i]);
+        }
+        printf("%d}\n", arr[9]);
+        testResult = -1;
+    }
+
+    for (int i = 0; i < 10; ++i)
+    {
+        arr[i] = 10 - i;
+    }
+    arr[6] = 10;
+
+    countSort(10, arr);
+    isTestFailed = 0;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        if ((i < 3 && arr[i] != i + 1) || (i == 3 && arr[i] != 5) || (i > 3 && i < 8 && arr[i] != i + 2) || (i >= 8 && arr[i] != 10))
+        {
+            isTestFailed = 1;
+            break;
+        }
+    }
+
+    if (isTestFailed)
+    {
+        printf("Test failed! Initial array = {10, 9, 8, 7, 6, 5, 10, 3, 2, 1}. Array after count sort: {");
+        for (int i = 0; i < 9; ++i)
+        {
+            printf("%d, ", arr[i]);
+        }
+        printf("%d}\n", arr[9]);
+        testResult = -1;
+    }
+
+    return testResult;
+}
+#endif
+
+int main()
+{
+#ifdef DEBUG
+    printf("Test result: %d\n", test());
+#endif
+    size_t arrSize = 0;
+    printf("Enter array size: ");
+    int scanResult = scanf("%lu", &arrSize);
+    while (scanResult == 0 || arrSize == 0)
+    {
+        scanf("%*[^\n]");
+        printf("Wrong input! Please enter natural number: ");
+        scanResult = scanf("%lu", &arrSize);
+    }
+
+    arrType *arr = malloc(arrSize * sizeof(int));
+    arrType *arr2 = malloc(arrSize * sizeof(int));
+    printf("Input array of numbers: ");
+    for (size_t i = 0; i < arrSize; ++i)
+    {
+        scanResult = scanf("%d", &arr[i]);
+        while (scanResult == 0)
+        {
+            scanf("%*[^\n]");
+            printf("Wrong input! Please enter numbers only: ");
+            scanResult = scanf("%d", &arr[i]);
+        }
+    }
+
+    for (size_t i = 0; i < arrSize; ++i)
+    {
+        arr2[i] = arr[i];
+    }
+
+    const clock_t clockAtStartBubble = clock();
+    bubbleSort(arrSize, arr);
+    const clock_t clockAtEndBubble = clock();
+
+    printf("Bubble sort took %.6f seconds! Sorted array:", (float)(clockAtEndBubble - clockAtStartBubble) / CLOCKS_PER_SEC);
+    for (size_t i = 0; i < arrSize; ++i)
+    {
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
+    free(arr);
+
+    const clock_t clockAtStartCount = clock();
+    countSort(arrSize, arr2);
+    const clock_t clockAtEndCount = clock();
+
+    printf("Count sort took %.6f seconds! Sorted array:", (float)(clockAtEndCount - clockAtStartCount) / CLOCKS_PER_SEC);
+    for (size_t i = 0; i < arrSize; ++i)
+    {
+        printf(" %d", arr2[i]);
+    }
+    printf("\n");
+    free(arr2);
+}
