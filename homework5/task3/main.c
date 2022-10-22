736347704ed55dbaa8726b5c856832857267397d
TimaFrolov <tima1911@yandex.ru>
2022-10-22 17:01:47 +0300

    Complete hw 5 task 3

---
+++ homework5/task3/main.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../stack/stack.h"
+
+int main()
+{
+    Stack *stack = NULL;
+    int errorCode = createStack(&stack, sizeof(char));
+    if (errorCode == -1)
+    {
+        printf("Error allocating memory!\n");
+        return -1;
+    }
+
+    printf("Input arithmetic equation in infix form (every number must contain only 1 digit) so programm will print it in postfix form: ");
+
+    for (char c = getc(stdin); c != '\n'; c = getc(stdin))
+    {
+        switch (c)
+        {
+        case ' ':
+            break;
+        case '+':
+        case '-':
+        {
+            char topValue = '\0';
+            int errorCode = stackPop(stack, &topValue);
+            while (errorCode == 0 && (topValue == '+' || topValue == '-'))
+            {
+                printf(" %c", topValue);
+                errorCode = stackPop(stack, &topValue);
+            }
+            if (errorCode == 0)
+            {
+                errorCode = stackPush(stack, &topValue);
+                if (errorCode == -1)
+                {
+                    printf("\nError allocating memory!\n");
+                    stackFree(stack);
+                    free(stack);
+                    return -1;
+                }
+            }
+            errorCode = stackPush(stack, &c);
+            if (errorCode == -1)
+            {
+                printf("\nError allocating memory!\n");
+                stackFree(stack);
+                free(stack);
+                return -1;
+            }
+            break;
+        }
+        case '*':
+        case '/':
+        {
+            char topValue = '\0';
+            int errorCode = stackPop(stack, &topValue);
+            while (errorCode == 0 && (topValue == '+' || topValue == '-' || topValue == '*' || topValue == '/'))
+            {
+                printf("%c", topValue);
+                errorCode = stackPop(stack, &topValue);
+            }
+            if (errorCode == 0)
+            {
+                errorCode = stackPush(stack, &topValue);
+                if (errorCode == -1)
+                {
+                    printf("\nError allocating memory!\n");
+                    stackFree(stack);
+                    free(stack);
+                    return -1;
+                }
+            }
+            errorCode = stackPush(stack, &c);
+            if (errorCode == -1)
+            {
+                printf("\nError allocating memory!\n");
+                stackFree(stack);
+                free(stack);
+                return -1;
+            }
+            break;
+        }
+        case '(':
+        {
+            errorCode = stackPush(stack, &c);
+            if (errorCode == -1)
+            {
+                printf("\nError allocating memory!\n");
+                stackFree(stack);
+                free(stack);
+                return -1;
+            }
+            break;
+        }
+        case ')':
+        {
+            char topValue = '\0';
+            int errorCode = stackPop(stack, &topValue);
+            while (errorCode == 0 && topValue != '(')
+            {
+                printf(" %c", topValue);
+                errorCode = stackPop(stack, &topValue);
+            }
+            if (errorCode == -3)
+            {
+                printf("\nIncorrect input!\n");
+                stackFree(stack);
+                free(stack);
+                return 1;
+            }
+            break;
+        }
+        default:
+        {
+            int num = c - '0';
+            if (num > 9 || num < 0)
+            {
+                printf("\nIncorrect input!\n");
+                stackFree(stack);
+                free(stack);
+                return 1;
+            }
+            printf(" %d", num);
+        }
+        }
+    }
+    char topValue = '\0';
+    errorCode = stackPop(stack, &topValue);
+    while (errorCode == 0)
+    {
+        printf(" %c", topValue);
+        errorCode = stackPop(stack, &topValue);
+    }
+    printf("\n");
+
+    return 0;
+}
