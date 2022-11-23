cb20d40fb5fc47971cce6fb081cb850f6d95d767
TimaFrolov <tima1911@yandex.ru>
2022-11-23 18:08:56 +0300

    Complete testWork 2 task 1

---
+++ testWork2/task1.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+int getNextDate(FILE *file)
+{
+    while (1)
+    {
+        short curChar = fgetc(file);
+        if (curChar == -1)
+        {
+            return -1;
+        }
+        if (curChar < '0' || curChar > '9')
+        {
+            continue;
+        }
+        ungetc(curChar, file);
+        int day = 0;
+        int month = 0;
+        int year = 0;
+        int scanResult = -1;
+        fscanf(file, "%d", &day);
+        if (getc(file) != '.' || day > 31 || day < 1)
+        {
+            continue;
+        }
+        scanResult = fscanf(file, "%d", &month);
+        if (scanResult == 0 || getc(file) != '.' || month > 12 || month < 1)
+        {
+            continue;
+        }
+        scanResult = fscanf(file, "%d", &year);
+        if (scanResult == 0 || year < 1)
+        {
+            continue;
+        }
+        return day + month * 100 + year * 10000;
+    }
+}
+
+int main()
+{
+    char filename[101] = {0};
+    printf("Enter file path (no longer than 100 symbols): ");
+    scanf("%s", filename);
+    if (filename[100] != 0)
+    {
+        printf("Incorrect input!\n");
+        return -1;
+    }
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("Cannot open file!\n");
+        return -2;
+    }
+
+    int maxDate = -1;
+
+    while (!feof(file))
+    {
+        int curDate = getNextDate(file);
+        if (curDate > maxDate)
+        {
+            maxDate = curDate;
+        }
+    }
+
+    printf("Max date found in given file: ");
+
+    int day = maxDate % 100;
+    maxDate /= 100;
+    int month = maxDate % 100;
+    maxDate /= 100;
+    int year = maxDate;
+    if (day < 10)
+    {
+        printf("0");
+    }
+    printf("%d.", day);
+    if (month < 10)
+    {
+        printf("0");
+    }
+    printf("%d.%d", month, year);
+
+}
