63da23bbdf10b0d43ccff91bbdf9d1c4c5b16c1a
TimaFrolov <tima1911@yandex.ru>
2022-11-23 18:10:45 +0300

    Complete testWork 2 task 2

---
+++ testWork2/task2/stack.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef enum
+{
+    OK = 0,
+    MemoryAllocationError = -1,
+    GivenPointerIsNULL = -2,
+    StackIsEmpty = -3,
+} Error;
+
+typedef struct Stack Stack;
+
+// Create stack, allocating for values elementSize bytes
+// Possible errors: MemoryAllocationErrors, GivenPointerIsNULL, OK
+Error createStack(Stack **stack);
+
+// Add an element to stack
+// Possible errors: MemoryAllocationErrors, GivenPointerIsNULL, OK
+Error stackPush(Stack *stack, int value);
+
+// Remove top element from stack, get it value
+// Possible errors: GivenPointerIsNULL, StackIsEmpty, OK
+Error stackPop(Stack *stack, int *value);
+
+// Remove all elements from stack
+// Possible errors: GivenPointerIsNULL, OK
+Error stackFree(Stack *stack);
+
+// Print stack into stdin
+// Possible errors: GivenPointerIsNULL, OK
+Error stackPrint(Stack *stack);
