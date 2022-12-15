6670174095560793c5eb29be63a40892a9ef6ad2
TimaFrolov <tima1911@yandex.ru>
2022-12-15 16:31:33 +0300

    Modify hw 4 task 1 so it satisfies given task

--- homework4/task1.c
+++ homework4/task1.c
@@ -1,13 +1,49 @@
 #include <stdio.h>
 #include <locale.h>
 
-void print_binary(int number)
+typedef struct
 {
-    unsigned int n = 1 << 31;
-    while (n)
+    unsigned char bit[32];
+} BinNumber;
+
+BinNumber intToNumber(int number)
+{
+    BinNumber num = {bit : {0}};
+    for (unsigned int n = 1 << 31, i = 31; n > 0; n >>= 1, --i)
     {
-        printf(n & number ? "1" : "0");
-        n >>= 1;
+        num.bit[i] = n & number ? 1 : 0;
+    }
+    return num;
+}
+
+int binNumberToInt(BinNumber number)
+{
+    int num = 0;
+    for (unsigned int n = 0; n < 32; ++n)
+    {
+        num |= (number.bit[n]) << n;
+    }
+    return num;
+}
+
+BinNumber sumBitNumbers(BinNumber number1, BinNumber number2)
+{
+    BinNumber answer = {bit : {0}};
+    answer.bit[0] = number1.bit[0] ^ number2.bit[0];
+    unsigned char t = number1.bit[0] && number2.bit[0];
+    for (unsigned int n = 1; n < 31; ++n)
+    {
+        answer.bit[n] = number1.bit[n] ^ number2.bit[n] ^ t;
+        t = (number1.bit[n] + number2.bit[n] + t) >= 2;
+    }
+    return answer;
+}
+
+void printBitNumber(BinNumber number)
+{
+    for (signed char i = 31; i >= 0; --i)
+    {
+        printf("%i", number.bit[i]);
     }
 }
 
@@ -34,16 +70,20 @@ int main()
         scanResult = scanf("%d", &secondNumber);
     }
 
+    BinNumber firstBitNumber = intToNumber(firstNumber);
+    BinNumber secondBitNumber = intToNumber(secondNumber);
+    BinNumber sumOfBitNumbers = sumBitNumbers(firstBitNumber, secondBitNumber);
+
     printf("Двоичное представление первого числа: ");
-    print_binary(firstNumber);
+    printBitNumber(firstBitNumber);
     printf("\n");
     printf("Двоичное представление второго числа: ");
-    print_binary(secondNumber);
+    printBitNumber(secondBitNumber);
     printf("\n");
     printf("Двоичное представление суммы чисел:   ");
-    print_binary(firstNumber + secondNumber);
+    printBitNumber(sumOfBitNumbers);
     printf("\n");
-    printf("Сумма чисел: %d\n", firstNumber + secondNumber);
+    printf("Сумма чисел: %d\n", binNumberToInt(sumOfBitNumbers));
 
     return 0;
 }
