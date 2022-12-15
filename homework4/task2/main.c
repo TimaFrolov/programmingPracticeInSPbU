9e4aef4e9353fb22310f8bd912fd5bf2a808e448
TimaFrolov <tima1911@yandex.ru>
2022-12-15 16:56:17 +0300

    Modify hw 4 task 2 so it reads array from file

--- homework4/task2/main.c
+++ homework4/task2/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "sort.h"
 
+#define PATH_MAX 4096
+
 int find(size_t len, int *arr)
 {
     int ans = 0;
@@ -35,36 +37,54 @@ int find(size_t len, int *arr)
 
 int main()
 {
-    size_t arrSize = 0;
-    printf("Enter array size: ");
-    int scanResult = scanf("%lu", &arrSize);
-    while (scanResult == 0 || arrSize == 0)
+    char filepath[PATH_MAX] = {'\0'};
+
+    printf("Enter file path (no longer than %d symbols)\nFile on given path should have array length at first, then array itself: ", PATH_MAX);
+    scanf("%s", filepath);
+    if (filepath[PATH_MAX - 1] != '\0')
     {
-        scanf("%*[^\n]");
-        printf("Wrong input! Please enter natural number: ");
-        scanResult = scanf("%lu", &arrSize);
+        printf("Given file path is too long!\n");
+        return -3;
+    }
+    FILE *file = fopen(filepath, "r");
+    if (file == NULL)
+    {
+        printf("Cannot open file on given path!\n");
+        return -4;
+    }
+
+    size_t arrSize = 0;
+    int scanResult = fscanf(file, "%lu", &arrSize);
+    if (scanResult == 0 || arrSize == 0)
+    {
+        printf("Given file contains incorrect data!\n");
+        fclose(file);
+        return -2;
     }
 
     int *arr = calloc(arrSize, sizeof(int));
     if (arr == NULL)
     {
         printf("Error allocating memory for array!\n");
+        fclose(file);
         return -1;
     }
-    printf("Input array of numbers: ");
     for (size_t i = 0; i < arrSize; ++i)
     {
         scanResult = scanf("%d", &arr[i]);
         while (scanResult == 0)
         {
-            scanf("%*[^\n]");
-            printf("Wrong input! Please enter numbers only: ");
-            scanResult = scanf("%d", &arr[i]);
+            printf("Given file contains incorrect data!\n");
+            fclose(file);
+            free(arr);
+            return -2;
         }
     }
+    fclose(file);
 
     sort(arrSize, arr);
 
     printf("Most common value in array: %d\n", find(arrSize, arr));
+    free(arr);
     return 0;
 }
